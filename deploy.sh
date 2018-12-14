f0ad5cca42c794767d5214124e843f582d41c288
Unknown <mail@mathyn.com>
2018-12-14 13:59:42 +0100

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 $(aws ecr get-login --no-include-email --region eu-west-1)
 
-ng build
+./docker/package.sh debian64
 
 docker build --no-cache -t smilo/smilo-quake-server:latest .
 docker tag smilo/smilo-quake-server:latest 462619610638.dkr.ecr.eu-west-1.amazonaws.com/smilo/smilo-quake-server:latest
